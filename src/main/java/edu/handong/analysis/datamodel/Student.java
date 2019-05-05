e78ebe221df9c74cc5744e8286bc3cd65912f1b7
user <user@DESKTOP-BGIC6EH>
2019-05-05 14:11:54 +0900

    commit

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -2,6 +2,15 @@ package edu.handong.analysis.datamodel;
 
 public class Student {
 
-	String name;
+	private String name;
 	
+	public Student(String Name)
+	{
+		
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
 }
